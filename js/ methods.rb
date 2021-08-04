730aef57bc3fabd0f5ddef5b204698e9d09df710
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-04 10:55:07 -0400

    completed

---
+++ js/ methods.rb
@@ -0,0 +1,19 @@
+def greet_programmer
+    puts "Hello, programmer!"
+end    
+
+def greet(name)
+   puts "Hello, #{name}!"
+end
+def greetWithDefault(name = "programmer") 
+    puts "Hello, #{name}!"
+end    
+
+def add(num1, num2) 
+     num1 + num2
+end  
+
+def halve(num)
+   num/2
+end   
+
